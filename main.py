99027890392420cb29bf413380cdc14cbb6aa2af
dydyamotya <matderevenayv@yandex.ru>
2021-10-21 09:59:43 +0300

    Add recovery and sensoring time counting

--- main.py
+++ main.py
@@ -21,22 +21,28 @@ logger = logging.getLogger(__name__)
 def count(rg, r0):
     return ((r0 / rg) ** np.sign(r0 - rg)) - 1
 
-def find_90_percent(data: np.ndarray, reper_air: float, reper_gas: float):
+
+def find_x_percent(init_data: pd.Series, air_time: float, gas_time: float, percent: float):
     """Возвращает значения точек, в которых значения сопротивлений равны 90%
     от конечных значений."""
-    # Сомнительная идея, сделать так, чтобы мы могли учитывать оба варианта
-    # расположения газов
-    # Пока делать не будем
-    # if reper_gas > reper_air:
-    # reper_gas, reper_air = reper_air, reper_gas
+    air = init_data.at[air_time]
+    gas = init_data.at[gas_time]
+    delta = air - gas
+    air_data = init_data.loc[:air_time]
+    gas_data = init_data.loc[air_time:]
+    # air_data - air + percent * (air - gas) = air_data - percent * gas - air * ( 1 - percent )
+    percent_air_time = air_data.index[np.abs(air_data - air + (1 - percent) * delta).argmin()]
+    percent_gas_time = gas_data.index[np.abs(gas_data - gas - (1 - percent) * delta).argmin()]
+    return percent_gas_time, percent_air_time
 
-    delta = reper_air - reper_gas
-    procent = 0.9 * delta
-    data_recovery = np.abs(data - (reper_gas + procent))
-    data_sens = np.abs(data - (reper_air - procent))
-    result1 = np.min(np.argsort(data_sens)[:20])
-    result2 = np.max(np.argsort(data_recovery)[:4])
-    return result1, result2
+def all_parametrs(data, t0, tg, percent):
+    rg = data.at[tg]
+    r0 = data.at[t0]
+    S = count(rg, r0)
+    t_gas, t_air = find_x_percent(data, t0, tg, percent)
+    logger.debug(f"{tg}, {t_gas}, {t0}, {t_air}")
+    return S, tg-t_gas, t0-t_air
+
 
 class WidgetsDict():
     def __init__(self, fields):
@@ -64,14 +70,15 @@ class MainWidget(QtWidgets.QWidget):
         self.ax.set_ylabel("R, Ohm")
         self.ax.set_yscale("log")
 
-
-        fields = ("produv", "gas1", "gas2", "delta", "file")
+        fields = ("produv", "gas1", "gas2", "delta", "percent", "file")
         self.widgets = WidgetsDict(fields)
-
 
         self.data = None
         self.gas1_lines = []
         self.gas2_lines = []
+        self.highlighted_region = None
+        self.gas_line = None
+        self.air_line = None
         self.produvka_line = self.ax.axvline(0)
 
         self._init_ui()
@@ -105,6 +112,9 @@ class MainWidget(QtWidgets.QWidget):
             form_layout.addRow(label, widget)
         self.setLayout(layout)
 
+        self.mark_region_cid = self.plot_widget.mpl_connect(
+            "button_press_event", self.mark_region)
+
     def open_file(self):
         filename, filters = QtWidgets.QFileDialog.getOpenFileName(self, "Open file", (pathlib.Path.home(
         )/"projects/repos/simple_laba_cut/tests/ignore").as_posix(), "DAT file (*.dat)")
@@ -118,7 +128,8 @@ class MainWidget(QtWidgets.QWidget):
 
     def read_file(self):
         try:
-            self.data = pd.read_csv(self.widgets["file"], decimal=',', skiprows=1, sep='\t', index_col="Time")
+            self.data = pd.read_csv(
+                self.widgets["file"], decimal=',', skiprows=1, sep='\t', index_col="Time")
         except:
             raise TypeError
         else:
@@ -145,184 +156,111 @@ class MainWidget(QtWidgets.QWidget):
             gas1_seconds = float(self.widgets["gas1"])
             gas2_seconds = float(self.widgets["gas2"])
         except ValueError:
-            return 
+            return
         else:
             self.produvka_line.set_xdata((produvka_seconds, produvka_seconds))
             onecyc = gas1_seconds + gas2_seconds
             max_time = max(self.data.index) - onecyc
             self.table.setRowCount(int((max_time - produvka_seconds)/onecyc))
-            self.table.setColumnCount(4)
+            self.table.setColumnCount(12)
             if self.gas1_lines or self.gas2_lines:
                 self.ax.collections = []
-            self.gas1_lines = self.ax.vlines([x for x in np.arange(produvka_seconds, max_time, onecyc) + gas1_seconds], *self.ax.get_ylim(), color="blue")
-            self.gas2_lines = self.ax.vlines([self.get_minimum_in_delta(x) for x in np.arange(produvka_seconds, max_time, onecyc) + onecyc], *self.ax.get_ylim(), color="red")
+            self.gas1_lines = self.ax.vlines([x for x in np.arange(
+                produvka_seconds, max_time, onecyc) + gas1_seconds], *self.ax.get_ylim(), color="blue")
+            self.gas2_lines = self.ax.vlines([self.get_minimum_in_delta(x) for x in np.arange(
+                produvka_seconds, max_time, onecyc) + onecyc], *self.ax.get_ylim(), color="red")
             self.plot_widget.draw()
             self.cut_full()
 
-
-
     def cut_full(self):
-        for idx, (gas1_segment, gas2_segment) in enumerate(zip(self.gas1_lines.get_segments(), self.gas2_lines.get_segments())):
-            r0 = self.data.iloc[self.data.index.get_loc(gas1_segment[0, 0], method="nearest")].loc[["R1", "R2", "R3", "R4"]].to_numpy()
-            rg = self.data.iloc[self.data.index.get_loc(gas2_segment[0, 0], method="nearest")].loc[["R1", "R2", "R3", "R4"]].to_numpy()
-            for idx2, S in enumerate(count(rg, r0)):
-                item = self.table.item(idx, idx2)
-                if not item:
-                    item = QtWidgets.QTableWidgetItem()
-                    self.table.setItem(idx, idx2, item)
-                item.setText("{:.3f}".format(S))
+        columns = ["R1", "R2", "R3", "R4"]
+        try:
+            percent = float(self.widgets["percent"])
+        except ValueError:
+            return
+        else:
+            for idx, (gas1_segment, gas2_segment) in enumerate(zip(self.gas1_lines.get_segments(), self.gas2_lines.get_segments())):
+                t0 = self.data.index[self.data.index.get_loc(
+                    gas1_segment[0, 0], method="nearest")]
+                tg = self.data.index[self.data.index.get_loc(
+                    gas2_segment[0, 0], method="nearest")]
+                _, left_x, right_x, intra_x = self.find_gas2_segment_borders(tg)
+                logger.debug(f"In cut full: t0:{t0}, tg:{tg}, left_x:{left_x}, right_x:{right_x}, intra_x:{intra_x}")
+
+                for idx2, column in enumerate(columns):
+                    for idx3, field in enumerate(all_parametrs(self.data.loc[left_x:right_x, column], t0,
+                                                               tg, percent)):
+                        item = self.table.item(idx, idx2 * 3 + idx3)
+                        if not item:
+                            item = QtWidgets.QTableWidgetItem()
+                            self.table.setItem(idx, idx2 * 3 + idx3, item)
+                        item.setText("{:.3f}".format(field))
 
     def save(self):
-        save_array = np.zeros((self.table.rowCount(), self.table.columnCount()))
+        save_array = np.zeros(
+            (self.table.rowCount(), self.table.columnCount()))
         for row in range(self.table.rowCount()):
             for col in range(self.table.columnCount()):
                 save_array[row, col] = float(self.table.item(row, col).text())
 
         file = pathlib.Path(self.widgets["file"]).with_suffix(".tsv")
-        pd.DataFrame(save_array, columns=["S{}".format(x) for x in range(1, 5)]).to_csv(file, sep='\t', index=False)
+        pd.DataFrame(save_array, columns=["S{}".format(x) for x in range(1, 5)]).to_csv(
+            file, sep='\t', index=False)
 
-
-    def cut_old(self):
+    def mark_region(self, event):
+        x, y = event.xdata, event.ydata
+        idx, left_x, right_x, intra_x = self.find_gas2_segment_borders(x)
+        tg = self.data.index[self.data.index.get_loc(
+                    right_x, method="nearest")]
+        t0 = self.data.index[self.data.index.get_loc(
+                    intra_x, method="nearest")]
+        if idx is None:
+            return
+        if self.highlighted_region:
+            self.ax.patches.remove(self.highlighted_region)
+        self.highlighted_region = self.ax.axvspan(
+            left_x, right_x, color="#FF000044")
 
         try:
-            _ = [value.get() for value in self.variables.values()]
-            self.message("Все ок")
-        except ValueError as e:
-            self.message("Не все поля заполнены, милорд")
-            print(e)
-        else:
-            data = pd.read_csv(
-                self.variables["file"].get(), decimal=',', skiprows=1, sep='\t')
-            freq = int(self.variables["freq"].get())
-            onecyc = int(self.variables["onecyc"].get()) * freq
-            produv = int(self.variables["prod"].get()) * freq
-            logger.debug(f"{freq}, {produv}, {onecyc}")
-
-            if self.produvka_index is None:
-                self.get_indexes(data.loc[:produv + onecyc + 200, "R1"], 1)
-                return None
-
-            data = data.iloc[self.produvka_index:]
-            onecyc = self.gas_index - self.produvka_index
-            logger.debug("Onecyc value: {}".format(onecyc))
-            data.index = np.arange(data.shape[0])
-            counter = 0
-            fd = self.output_filename.open("w")
-            for sensor_column_name in ("R{}".format(i) for i in range(1, 13)):
-                try:
-                    data[sensor_column_name]
-                except KeyError:
-                    break
-                else:
-                    if sensor_column_name == "R1":
-                        fd.write("Rg,R0,S,T_res,T_rec")
-                    else:
-                        fd.write(",Rg,R0,S,T_res,T_rec")
-            while True:
-                temp_data = data.iloc[counter * onecyc: (counter + 1) * onecyc]
-                if temp_data.shape[0] != 0:
-                    print(temp_data.shape, end=" ")
-                    for sensor_column_name in ("R{}".format(i) for i in range(1, 13)):
-                        try:
-                            temp_series = temp_data[sensor_column_name]
-                        except KeyError:
-                            continue
-                        else:
-                            if (self.idx1 is None) or (self.idx2 is None):
-                                self.get_indexes(temp_series, 2)
-                                fd.close()
-                                return
-                            try:
-                                rg = temp_series.iloc[self.idx1]
-                                r0 = temp_series.iloc[self.idx2]
-                            except IndexError:
-                                continue
-                            else:
-                                s = count(rg, r0)
-                                time_idx_1, time_idx_2 = self.find_90_percent(
-                                    temp_series.values, r0, rg)
-                                t_sens = time_idx_1 - 0
-                                t_recovery = time_idx_2 - self.idx1
-                                format_line = "{:3.4f},{:3.4f},{:3.4f},{:3.4f},{:3.4f}"
-                                if sensor_column_name == "R1":
-                                    fd.write(
-                                        ("\n" + format_line).format(rg, r0, s, t_sens, t_recovery))
-                                else:
-                                    fd.write(
-                                        ("," + format_line).format(rg, r0, s, t_sens, t_recovery))
-                    counter += 1
-                else:
-                    fd.write("\n")
-                    fd.close()
-                    break
-
-
-
-
-class MatplotlibCutWidget():
-    def __init__(self, data: pd.Series, func: Callable, message_func: Callable):
-        super().__init__()
-        self.func = func
-        self.message_func = message_func
-        self.f = Figure(figsize=(5, 5), dpi=100)
-        self.ax = self.f.add_subplot(111)
-        self.ax.plot(data.index, data.values)
-        self.ax.set_yscale("log")
-
-        self.canvas = FigureCanvasTkAgg(self.f, self)
-        self.canvas.draw()
-        self.canvas.get_tk_widget().pack(fill=tk.X)
-        self.canvas.mpl_connect("button_press_event", self.onclick)
-
-        proceed_button = tk.Button(self, text="Резать резать резать дальше")
-        proceed_button["command"] = self.proceed
-        proceed_button.pack(fill=tk.X)
-
-        self.idx1 = None
-        self.idx2 = None
-        self.red_line = None
-        self.green_line = None
-        self.red_line_text = None
-        self.green_line_text = None
-
-    def onclick(self, event):
-        if event.button == MouseButton.LEFT:
-            self.idx1 = int(event.xdata)
-            if self.red_line is not None:
-                self.red_line.set_xdata(event.xdata)
-                self.red_line_text.set_text("{}".format(event.xdata))
-                self.red_line_text.set_x(event.xdata)
-            else:
-                self.red_line = self.ax.axvline(event.xdata, color='r', lw=0.5)
-                self.red_line_text = self.ax.text(event.xdata,
-                                                  self.ax.get_ylim()[0], "{}".format(event.xdata))
-        elif event.button == MouseButton.RIGHT:
-            self.idx2 = int(event.xdata)
-            if self.green_line is not None:
-                self.green_line.set_xdata(event.xdata)
-                self.green_line_text.set_text("{}".format(event.xdata))
-                self.green_line_text.set_x(event.xdata)
-            else:
-                self.green_line = self.ax.axvline(
-                    event.xdata, color='g', lw=0.5)
-                self.green_line_text = self.ax.text(event.xdata,
-                                                    self.ax.get_ylim()[0], "{}".format(event.xdata))
-        else:
+            percent = float(self.widgets["percent"])
+        except ValueError:
             pass
-        self.canvas.draw()
+        else:
+            t_gas, t_air = find_x_percent(self.data.loc[left_x:right_x, "R1"], t0, tg, percent)
+            logger.debug(f"In mark: {t0} {t_air} {tg} {t_gas} {left_x} {right_x}")
+            if self.gas_line:
+                self.ax.lines.remove(self.gas_line)
+            self.gas_line = self.ax.axvline(t_gas, color="red", ls='--')
+            if self.air_line:
+                self.ax.lines.remove(self.air_line)
+            self.air_line = self.ax.axvline(t_air, color="blue", ls='--')
+        finally:
+            logger.debug("Region is drawn")
+            self.plot_widget.draw()
 
-    def get_indexes(self):
-        return map(int, (self.idx1, self.idx2))
+    def find_gas2_segment_borders(self, x):
+        if not self.gas2_lines:
+            return None, None, None
 
-    def proceed(self):
-        idx1, idx2 = self.get_indexes()
-        logger.debug(f"{idx1} {idx2}")
-        self.message_func("Indexes = {:d} {:d}".format(idx1, idx2))
-        self.func(indexes=(idx1, idx2))
-        self.destroy()
+        prev_idx, prev_x = None, None
+        iterator = iter(
+                enumerate(zip(map(lambda x: x[0, 0], self.gas1_lines.get_segments()), map(lambda x: x[0, 0], self.gas2_lines.get_segments() ) )))
+        while True:
+            try:
+                idx, (segment_x_gas, segment_x) = next(iterator)
+            except StopIteration:
+                return None, None, None
+            if x <= segment_x:
+                break
+            else:
+                prev_idx, prev_x = idx, segment_x
+        if prev_idx is None:
+            return -1, self.produvka_line.get_xdata()[0], segment_x, segment_x_gas
+        else:
+            return prev_idx, prev_x, segment_x, segment_x_gas
 
 
-if __name__ == "__main__":
+def main():
     parser = argparse.ArgumentParser()
     parser.add_argument("--debug", action="store_true")
     args = parser.parse_args()
@@ -332,3 +270,7 @@ if __name__ == "__main__":
     widget = MainWidget()
     widget.show()
     sys.exit(app.exec_())
+
+
+if __name__ == "__main__":
+    main()
